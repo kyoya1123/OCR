9b121f15f07579eefbd1407f5c6254a8a51cd00c
響也山口 <kyoryu32@gmail.com>
2016-05-08 14:53:17 +0900

    Initial Commit

---
+++ OCR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCR
+//
+//  Created by Family Account on 2016/05/08.
+//  Copyright © 2016年 Family Account. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
